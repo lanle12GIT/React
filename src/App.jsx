3f7dd4420a9af742f5deaffc3509374be3e0ae08
Lan Le <lanle120801@gmail.com>
2025-03-04 01:26:35 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+const myFunction = ()=> {
+ console.log(">>> rum my arrow function")
+}
+myFunction(); 
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      hhhhh
+    </>
+  )
+}
+
+export default App
